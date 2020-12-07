e737fe0068735d3a1669c62f839d90fbc01b4ddf
taeyoung98 <119taeyoung@naver.com>
2020-12-08 05:05:55 +0900

    1st challenge

---
+++ challenge/1/index.js
@@ -0,0 +1,40 @@
+class ObjectUtilities {
+  /* Your magic here */
+
+  static mergeObjects = (a, b) => {
+    return { ...a, ...b };
+  };
+  static removePassword = (obj) => {
+    const name = obj["name"];
+    const favFood = obj["favFood"];
+    return { name, favFood };
+  };
+  static freezeObj = (obj) => Object.freeze(obj);
+  static getOnlyValues = (obj) => Object.values(obj);
+  static getOnlyProperties = (obj) => Object.keys(obj);
+}
+
+const objA = {
+  name: "Nicolas",
+  favFood: "Kimchi",
+};
+
+const objB = {
+  password: "12345",
+};
+
+const user = ObjectUtilities.mergeObjects(objA, objB);
+console.log(user);
+
+const cleanUser = ObjectUtilities.removePassword(user);
+console.log(cleanUser);
+
+const frozenUser = ObjectUtilities.freezeObj(cleanUser);
+
+const onlyValues = ObjectUtilities.getOnlyValues(frozenUser);
+console.log(onlyValues);
+
+const onlyProperties = ObjectUtilities.getOnlyProperties(frozenUser);
+console.log(onlyProperties);
+
+frozenUser.name = "Hello!"; // This should show an error
