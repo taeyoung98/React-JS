0a40a23521797383a7fac10e0e69a7fb394ef180
taeyoung98 <119taeyoung@naver.com>
2020-12-09 23:10:33 +0900

    2nd challenge :package:

--- challenge/2/index.js
+++ challenge/2/index.js
@@ -4,8 +4,16 @@ class ArrayUtilities {
   static addZeros = (numbers) => numbers.map((num) => num * 10);
   static moreThanFifty = (addZeros) => addZeros.filter((num) => num > 50);
   static removeFirst = (moreThanFifty) => moreThanFifty.slice(1);
+
+  // Solution
+  // static removeFirst = (arr) => {
+  //   const [, ...rest] = arr;
+  //   return rest;
+  // };
+
   static sumAll = (noFist) => noFirst.reduce((accumulator, currentValue) => accumulator + currentValue);
   static divide = (sumAll) => Array.from(`${sumAll}`);
+  // Solution: static divide = payload => `${payload}`.split("");
 }
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
